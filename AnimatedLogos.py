7b7a3ad43ba5b1c2114d9dd5fb596bf7faae1f37
Функция f(x) <murkotyara93@gmail.com>
2025-10-01 09:05:21 +0000

    Автоматический коммит после рендера

--- AnimatedLogos.py
+++ AnimatedLogos.py
@@ -30,16 +30,13 @@ class ParabolaLogo(MovingCameraScene):
         self.camera.frame.set(height=9)
         self.play(Create(plane), run_time=3)
 
-        def speed_func(t):
-            return (t-0.5)**2 + 0.1
-
         def rate_func(speed_func, t):
             ts = np.linspace(0,1,200)
             s = np.cumsum(speed_func(ts))
             s = (s - s[0]) / (s[-1]-s[0])
             return np.interp(t, ts, s)
         
-        self.play(Create(parabola), rate_func=lambda t: rate_func(speed_func=speed_func, t=t))
+        self.play(Create(parabola), rate_func=lambda t: rate_func(lambda x: (x-0.5)**2 + 0.1, t))
         self.play(Create(label))
         self.play(Create(text))
         self.play(Create(border))
@@ -73,7 +70,14 @@ class ExponentLogo(MovingCameraScene):
         border = Circle(color=BLUE, stroke_width=5000, radius=29.48)
         self.camera.frame.set(height=9)
         self.play(Create(plane), run_time=3)
-        self.play(Create(exponent), run_time=8/3)
+
+        def rate_func(speed_func, t):
+            ts = np.linspace(0,1,200)
+            s = np.cumsum(speed_func(ts))
+            s = (s - s[0]) / (s[-1]-s[0])
+            return np.interp(t, ts, s)
+        
+        self.play(Create(exponent), run_time=8/3, rate_func=lambda t: rate_func(lambda x: 10000^(x+1), t))
         self.play(Create(label))
         self.play(Create(text))
         self.play(Create(border))
